d2216be630d578a1f06e1b0573a6f1a2a1c2290a
Cecilia Lopez <andreacecilopez88@gmail.com>
2025-05-07 16:19:54 -0300

    added object instances and verified methods

--- src/com/listentome/main/Main.java
+++ src/com/listentome/main/Main.java
@@ -1,6 +1,7 @@
 package com.listentome.main;
 
 import com.listentome.models.Cancion;
+import com.listentome.models.MisFavoritos;
 import com.listentome.models.Podcast;
 
 public class Main {
@@ -16,20 +17,34 @@ public class Main {
 //        System.out.println("Duracion: " + cancion1.getDuracion() + " minutos");
 
         Podcast podcast1 = new Podcast();
-        podcast1.setTitulo("");
+        podcast1.setTitulo("Mentes Creativas");
         podcast1.setDuracion(60);
-        podcast1.setPresentador("");
-        podcast1.setDescripcion("");
+        podcast1.setPresentador("Javier López");
+        podcast1.setDescripcion("Mentes Creativas es un podcast donde exploramos el fascinante mundo de la creatividad en todas sus formas. Cada semana, Javier López entrevista a artistas, diseñadores y pensadores innovadores que comparten sus experiencias, procesos creativos y consejos prácticos para inspirar a quienes buscan liberar su potencial creativo. Únete a nosotros para descubrir nuevas ideas, técnicas y la magia que se esconde detrás de cada obra maestra.");
 
+        //cancion
         for (int i = 0; i < 100; i++) {
             cancion1.meGusta();
         }
 
-        for (int i = 0; i < 5000; i++) {
+        for (int i = 0; i < 10000; i++) {
             cancion1.reproduce();
+        }
+
+        //podcast
+        for (int i = 0; i < 100; i++) {
+            podcast1.meGusta();
+        }
+
+        for (int i = 0; i < 5000; i++) {
+            podcast1.reproduce();
         }
 
         System.out.println("Total de reproducciones: " + cancion1.getTotalDeReproducciones());
         System.out.println("Total de me gusta: " + cancion1.getTotalMeGusta());
+
+        MisFavoritos favoritos = new MisFavoritos();
+        favoritos.adicione(podcast1);
+        favoritos.adicione(cancion1);
     }
 }
